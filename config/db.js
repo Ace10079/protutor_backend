38a4690aa838c1f83871a325b1e24e1ac3ac2ca1
Rk-Dinesh <rdkumar881@gmail.com>
2024-03-25 16:43:34 +0530

    host ip

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://localhost:27017/tutor_finder').on('open',() => {
+const connection = mongoose.createConnection('mongodb://ProtutorAdmin:ProTutor2024@13.54.14.73:27017/tutor_finder?directConnection=true').on('open',() => {
     console.log("MongoDB Connected");
 }).on('error',() =>{
     console.log("MongoDB Connection error");
